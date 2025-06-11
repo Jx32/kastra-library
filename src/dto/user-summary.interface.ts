4b3578beb1050a83e3e4219533fa859dcf7520b8
Jorge Fernández <jorgeefernandezz@gmail.com>
2025-06-10 19:30:58 -0600

    New version 8a7f537da0dafcc3a23b8fcd9a52420a679c8119

--- src/dto/user-summary.interface.ts
+++ src/dto/user-summary.interface.ts
@@ -11,7 +11,6 @@ export interface UserSummary {
 export const userSummarySchema = z.object({
     remoteGates: z.array(remoteGateSchema),
     currentPinAccess: z.string().length(4, "Current PIN must be 4 digits length"),
-    status: z.enum(["active", "inactive"]),
     topicName: z.string().min(1, "Topic name cannot be empty"),
 }).strict();
 
