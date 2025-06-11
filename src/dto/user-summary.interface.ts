8a7f537da0dafcc3a23b8fcd9a52420a679c8119
Jorge Fernández <jorgeefernandezz@gmail.com>
2025-06-10 19:30:40 -0600

    New version 73f7b96184b090c2b3eaca0f753180132cd5190c

--- src/dto/user-summary.interface.ts
+++ src/dto/user-summary.interface.ts
@@ -4,12 +4,15 @@ import { RemoteGate, remoteGateSchema } from "./remote-gate.interface";
 export interface UserSummary {
     remoteGates: RemoteGate[];
     currentPinAccess: string;
+    topicName: string;
     // TODO: Add payment summary object
 }
 
 export const userSummarySchema = z.object({
     remoteGates: z.array(remoteGateSchema),
-    currentPinAccess: z.string().length(4, "Current PIN must be 4 digits length")
+    currentPinAccess: z.string().length(4, "Current PIN must be 4 digits length"),
+    status: z.enum(["active", "inactive"]),
+    topicName: z.string().min(1, "Topic name cannot be empty"),
 }).strict();
 
 export type UserSummaryType = z.infer<typeof userSummarySchema>;
