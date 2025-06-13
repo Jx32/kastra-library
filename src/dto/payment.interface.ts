b7742257d254706db4a1292c2c7b3595d6de9090
Jorge Fernández <jorgeefernandezz@gmail.com>
2025-06-12 18:53:33 -0600

    New version abb3966be4383348fafc8b47eb396ba61ea8b886

---
+++ src/dto/payment.interface.ts
@@ -0,0 +1,33 @@
+/**
+ * RemoteDevice Interface represents a gate that can be controlled remotely,
+ * and are installed physically at the entrance or exit of a residential area.
+ * 
+ * @interface RemoteGate
+ */
+
+import { z } from "zod";
+import { ObjectId } from "mongodb";
+
+export interface Payment {
+    _id?: string;
+    userSub: string;
+    amount: number;
+    currency: string;
+    status: "pending" | "completed" | "failed";
+    createdAt: string;
+    updatedAt: string;
+    stripePaymentId?: string; // Optional field for Stripe payment ID
+}
+
+export const PaymentSchema = z.object({
+    _id: z.string().transform(val => new ObjectId(val)).optional(),
+    userSub: z.string().uuid(),
+    amount: z.number(),
+    currency: z.string(),
+    status: z.enum(["pending", "completed", "failed"]),
+    createdAt: z.date(),
+    updatedAt: z.date(),
+    stripePaymentId: z.string().optional() // Optional field for Stripe payment ID
+});
+
+export type PaymentType = z.infer<typeof PaymentSchema>;
