f4b6ab243333cfb2db461e624f3b0381410fcb1a
Jorge Fernández <jorgeefernandezz@gmail.com>
2025-06-30 15:33:56 -0600

    New version 1473657d32fdcd1cae6705efe68bd065e2894491

--- src/dto/user-balance.interface.ts
+++ src/dto/user-balance.interface.ts
@@ -2,5 +2,5 @@ export interface UserBalance {
     balance: number; // Current balance of the user
     currency: string; // Currency of the balance, e.g., "USD", "EUR"
     // Status were extracted from Stripe documentation (https://docs.stripe.com/api/subscriptions/object?api-version=2025-05-28.basil#subscription_object-status)
-    subscriptionStatus: "incomplete" | "incomplete_expired" | "trialing" | "active" | "past_due" | "canceled" | "unpaid" | "paused"; // Status of the user's subscription
+    hasOverdueInvoices: boolean; // Indicates if the user has overdue invoices
 }
