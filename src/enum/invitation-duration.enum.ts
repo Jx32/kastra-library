d4463b2c5f6e9b7f2b2970f0591ceeaf63581565
Jorge Fernández <jorgeefernandezz@gmail.com>
2025-07-11 14:26:40 -0600

    New version ae8355ef61dd46e810ca98315bd81a8b6223d893

--- src/enum/invitation-duration.enum.ts
+++ src/enum/invitation-duration.enum.ts
@@ -12,18 +12,3 @@ export const enum InvitationDurationEnum {
     ONE_MONTH = "1 month",
     THREE_MONTHS = "3 months",
 }
-
-export enum InvitationDurationEnumValues {
-    ONE_HOUR = "1 hour",
-    TWO_HOURS = "2 hours",
-    FOUR_HOURS = "4 hours",
-    SIX_HOURS = "6 hours",
-    TWELVE_HOURS = "12 hours",
-    ONE_DAY = "1 day",
-    TWO_DAYS = "2 days",
-    THREE_DAYS = "3 days",
-    ONE_WEEK = "1 week",
-    TWO_WEEKS = "2 weeks",
-    ONE_MONTH = "1 month",
-    THREE_MONTHS = "3 months",
-}
