e80818e06e6b4e7728e7b4353b52b19a5ce51f6f
Jorge Fernández <jorgeefernandezz@gmail.com>
2025-07-27 11:41:31 -0600

    New version c597e4bdc08121647cfc793b5a8f3a7ebd57056f

--- src/dto/reset-password.ts
+++ src/dto/reset-password.ts
@@ -4,14 +4,6 @@ export interface ResetPassword {
     username: string;
 }
 
-/*
-Contains at least 1 number
-Contains at least 1 special character
-Contains at least 1 uppercase letter
-Contains at least 1 lowercase letter
-Contains at least 8 characters
-*/
-
 export const resetPasswordSchema = z.object({
     username: z.string().min(1, "Username is required"),
 });
