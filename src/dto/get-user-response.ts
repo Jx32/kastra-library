a12b2665dba45fee412021013e9cdc84aa74fb61
Jorge Fernández <jorgeefernandezz@gmail.com>
2025-07-28 16:01:22 -0600

    New version 9a048e81e48b7339587886ac51da7af610446070

--- src/dto/get-user-response.ts
+++
@@ -1,6 +0,0 @@
-import { User } from "./user.interface";
-
-export interface GetUserResponse {
-    users: User[],
-    paginationToken?: string,
-}
--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,6 @@
 export * from "./constants/constants";
 
 export * from "./dto/user.interface";
-export * from "./dto/get-user-response";
 export * from "./dto/patch-user.interface"
 export * from "./dto/delete-user-response";
 export * from "./dto/reset-password";
